637a5fdcf99b04e730678e770d1a533eef7f7c17
johnmaxrin <johnmaxrin@gmail.com>
2026-06-15 06:38:19 -0700

    Hoist all BMOVS_J and BMOVT_J to the top of the function

--- llvm/lib/Target/RISCV/RISCVNSOpt.cpp
+++ llvm/lib/Target/RISCV/RISCVNSOpt.cpp
@@ -25,6 +25,145 @@ using namespace llvm;
 #define DEBUG_TYPE "riscv-ns-branch-opt"
 #define PASS_NAME "riscv-ns-branch-opt"
 
+class BranchGroupInfo {
+public:
+  struct Group {
+    MachineInstr *BMOVS = nullptr;
+    MachineInstr *BMOVT = nullptr;
+    MachineInstr *BMOVC = nullptr;
+    MachineInstr *PB    = nullptr;
+    MachineBasicBlock *MBB = nullptr;
+  };
+
+  // Builds groups AND assigns/rewrites fresh branch registers
+  // as conflicts are detected.
+  void build(MachineFunction &MF) {
+    Groups.clear();
+    InstrToGroupIdx.clear();
+    UsedBRs.clear();
+
+    for (MachineBasicBlock &MBB : MF) {
+      for (MachineInstr &MI : MBB) {
+        if (MI.getOpcode() != RISCV::BMOVS_J)
+          continue;
+
+        Group G;
+        G.MBB  = &MBB;
+        G.BMOVS = &MI;
+
+        MCRegister OldReg = MI.getOperand(0).getReg().asMCReg();
+
+        // --- Find BMOVT_J with the same register ---
+        auto It = MI.getIterator();
+        for (++It; It != MBB.end(); ++It) {
+          if (It->getOpcode() == RISCV::BMOVT_J && hasReg(*It, OldReg)) {
+            G.BMOVT = &*It;
+            break;
+          }
+        }
+
+        // --- From there, find BMOVC and PB with the same register ---
+        if (G.BMOVT) {
+          auto It2 = G.BMOVT->getIterator();
+          for (++It2; It2 != MBB.end(); ++It2) {
+            if (!G.BMOVC && isBMOVC(It2->getOpcode()) && hasReg(*It2, OldReg)) {
+              G.BMOVC = &*It2;
+              continue;
+            }
+            if (!G.PB && RISCVNonSpec::isPB(It2->getOpcode()) &&
+                hasReg(*It2, OldReg)) {
+              G.PB = &*It2;
+              break;
+            }
+          }
+        }
+
+        // --- Register reassignment for this group ---
+        if (UsedBRs.count(OldReg)) {
+          MCRegister NewReg;
+          bool Found = false;
+          for (MCPhysReg Candidate : RISCV::PBRRegClass) {
+            if (!UsedBRs.count(Candidate)) {
+              NewReg = Candidate;
+              Found = true;
+              break;
+            }
+          }
+
+          if (!Found) {
+            LLVM_DEBUG(dbgs() << "BR pool exhausted for group in "
+                              << printMBBReference(MBB) << "\n");
+            // Leave OldReg as-is; group is still recorded below.
+          } else {
+            UsedBRs.insert(NewReg);
+            for (MachineInstr *Member : {G.BMOVS, G.BMOVT, G.BMOVC, G.PB}) {
+              if (!Member) continue;
+              for (MachineOperand &MO : Member->operands())
+                if (MO.isReg() && MO.getReg() == OldReg)
+                  MO.setReg(NewReg);
+            }
+          }
+        } else {
+          UsedBRs.insert(OldReg);
+        }
+
+        // --- Record the group ---
+        unsigned Idx = Groups.size();
+        Groups.push_back(G);
+        Group &Stored = Groups.back();
+        if (Stored.BMOVS) InstrToGroupIdx[Stored.BMOVS] = Idx;
+        if (Stored.BMOVT) InstrToGroupIdx[Stored.BMOVT] = Idx;
+        if (Stored.BMOVC) InstrToGroupIdx[Stored.BMOVC] = Idx;
+        if (Stored.PB)    InstrToGroupIdx[Stored.PB]    = Idx;
+      }
+    }
+  }
+
+  Group *getGroup(MachineInstr *MI) {
+    auto It = InstrToGroupIdx.find(MI);
+    if (It == InstrToGroupIdx.end())
+      return nullptr;
+    return &Groups[It->second];
+  }
+
+  MachineInstr *getBMOVS(MachineInstr *MI) { auto *G = getGroup(MI); return G ? G->BMOVS : nullptr; }
+  MachineInstr *getBMOVT(MachineInstr *MI) { auto *G = getGroup(MI); return G ? G->BMOVT : nullptr; }
+  MachineInstr *getBMOVC(MachineInstr *MI) { auto *G = getGroup(MI); return G ? G->BMOVC : nullptr; }
+  MachineInstr *getPB(MachineInstr *MI)    { auto *G = getGroup(MI); return G ? G->PB    : nullptr; }
+
+  MutableArrayRef<Group> groups() { return Groups; }
+
+private:
+  static bool isBMOVC(unsigned Opc) {
+    switch (Opc) {
+    case RISCV::BMOVC_BNE:
+    case RISCV::BMOVC_BEQ:
+    case RISCV::BMOVC_BGE:
+    case RISCV::BMOVC_BGEU:
+    case RISCV::BMOVC_BLT:
+    case RISCV::BMOVC_BLTU:
+    case RISCV::BMOVC_BITS:
+    case RISCV::BMOVC_LOOP:
+      return true;
+    default:
+      return false;
+    }
+  }
+
+  static bool hasReg(MachineInstr &MI, MCRegister Reg) {
+    for (MachineOperand &MO : MI.operands())
+      if (MO.isReg() && MO.getReg().isValid() &&
+          MO.getReg().asMCReg() == Reg)
+        return true;
+    return false;
+  }
+
+  SmallVector<Group, 16> Groups;
+  DenseMap<MachineInstr *, unsigned> InstrToGroupIdx;
+  SmallSet<MCRegister, 32> UsedBRs;
+};
+
+
 namespace {
 class RISCVNSBranchOpt : public MachineFunctionPass {
 
@@ -33,213 +172,43 @@ public:
   RISCVNSBranchOpt() : MachineFunctionPass(ID) {}
 
   bool runOnMachineFunction(MachineFunction &MF) override {
+  BranchGroupInfo BGI;
+  BGI.build(MF);   // builds groups AND renames registers in-place
 
+  MachineBasicBlock &EntryBlk = MF.front();
+  MachineBasicBlock::iterator InsertionPt = EntryBlk.getFirstNonPHI();
 
-    //  ----- Do the mapping first ------ 
-    // ---- Let's create a proper structure ---- 
+  bool Changed = false;
 
-    DenseMap<MachineInstr*, MachineInstr *> BMOVSMap;
-    DenseMap<MachineInstr*, MachineInstr *> BMOVTMap;
-    SmallSet<MCRegister, 32> UsedBRs; 
-    for(MachineBasicBlock &MBB : MF)
-    {
-      for(MachineInstr &MI: MBB)
-      {
-        if(MI.getOpcode() != RISCV::BMOVS_J && MI.getOpcode() != RISCV::BMOVT_J)
-          continue;
+  // Build BMOVSMap / BMOVTMap from the groups
+  DenseMap<MachineInstr*, MachineInstr*> BMOVSMap;
+  DenseMap<MachineInstr*, MachineInstr*> BMOVTMap;
 
-        Register Reg = MI.getOperand(0).getReg();
+  for (auto &G : BGI.groups()) {
+    if (G.PB && G.BMOVS) BMOVSMap[G.PB] = G.BMOVS;
+    if (G.PB && G.BMOVT) BMOVTMap[G.BMOVT] = G.PB;
 
-        for(auto It = MI.getIterator(); It != MBB.end(); ++It)
-        {
-          if(!RISCVNonSpec::isPB(It->getOpcode())) continue;
-          if(It->getOperand(0).getReg() != Reg) continue;
-
-          if(MI.getOpcode() == RISCV::BMOVS_J)
-            BMOVSMap[&*It] = &MI;
-          else
-            BMOVTMap[&MI] = &*It;  // [Change Later] For the time being do this. Change this later.
-        }
-
-      }
+    // Hoist BMOVS_J / BMOVT_J to entry, now correctly renamed
+    if (G.BMOVS) {
+      EntryBlk.splice(InsertionPt, G.MBB, G.BMOVS->getIterator());
+      Changed = true;
     }
-
-    MF.getInfo<RISCVMachineFunctionInfo>()->setBMOVSMap(std::move(BMOVSMap));
-    MF.getInfo<RISCVMachineFunctionInfo>()->setBMOVTMap(std::move(BMOVTMap));
-    // ----- End of mapping -----
-
-    bool Changed = false;
-    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
-    const TargetRegisterInfo *TRI = MF.getSubtarget().getRegisterInfo();
-
-    // Get insertion point for bmovs_j and bmovt_j
-    MachineBasicBlock &EntryBlk  = MF.front();
-
-    // LLVM_DEBUG(dbgs() << "RISCV Non Speculative Branch Optimization\n");
-
-    for (MachineBasicBlock &MBB : MF) {
-      Changed |= optimizeBlock(MBB, TII, EntryBlk, UsedBRs);
+    if (G.BMOVT) {
+      EntryBlk.splice(InsertionPt, G.MBB, G.BMOVT->getIterator());
+      Changed = true;
     }
-
-    
-
-
-    // --- End of register allocation -----
-
-    return Changed;
   }
+
+  MF.getInfo<RISCVMachineFunctionInfo>()->setBMOVSMap(std::move(BMOVSMap));
+  MF.getInfo<RISCVMachineFunctionInfo>()->setBMOVTMap(std::move(BMOVTMap));
+
+  return Changed;
+}
 
   StringRef getPassName() const override {
     return "RISCV NS Branch Optimization";
   }
-
-private:
-  bool optimizeBlock(MachineBasicBlock &MBB, const TargetInstrInfo *TII, MachineBasicBlock  &EntryBlk, SmallSet<MCRegister, 32> &UsedBRs) {
-    bool Changed = true;
-    int Cbmovs = 0;
-    int Cbmovt = 0;
-    int Cbmovc = 0;
-    int Cpb = 0;
-
-
-    //MachineBasicBlock::iterator InsertionPt = EntryBlk.getFirstTerminator();
-    MachineBasicBlock::iterator InsertionPt = EntryBlk.getFirstNonPHI();
-    MachineRegisterInfo &MRI = MBB.getParent()->getRegInfo();
-
-    MachineFunction *MF = EntryBlk.getParent();
-
-    for (auto I = MBB.begin(), E = MBB.end(); I != E;) {
-      MachineInstr &MI = *I++;
-
-      // LLVM_DEBUG(
-      //     { dbgs() << "Opcode: " << TII->getName(MI.getOpcode()) << "\n"; });
-
-      switch (MI.getOpcode()) {
-     case RISCV::BMOVS_J:
-        {
-          ++Cbmovs;
-
-          if (MI.getOperand(0).isReg() && MI.getOperand(0).isDef()) {
-            MCRegister OldReg = MI.getOperand(0).getReg().asMCReg();
-
-            if (UsedBRs.count(OldReg)) {
-              // Already claimed by a previously hoisted pair -> need a fresh reg
-              LLVM_DEBUG(dbgs() << "New Reg: \n";);
-              MCRegister NewReg;
-              bool Found = false;
-              for (MCPhysReg Candidate : RISCV::PBRRegClass) {
-                if (!UsedBRs.count(Candidate)) {
-                  NewReg = Candidate;
-                  Found = true;
-                  break;
-                }
-              }
-
-              if (!Found) {
-                LLVM_DEBUG(dbgs() << "BR pool exhausted, skipping: "; MI.dump());
-              } else {
-                UsedBRs.insert(NewReg);
-                MI.getOperand(0).setReg(NewReg);
-
-                LLVM_DEBUG(dbgs() << "Updating: "; MI.dump());
-                // Create the MAP and only update the operand of those pairs. 
-                for (MachineBasicBlock &SearchMBB : *MF)
-
-                  for (MachineInstr &SearchMI : SearchMBB)
-                    for (MachineOperand &MO : SearchMI.operands())
-                      if (MO.isReg() && MO.getReg() == OldReg && &SearchMI != &MI)
-                        MO.setReg(NewReg);
-              }
-            } else {
-              // First time this register is being hoisted - keep it as-is
-              UsedBRs.insert(OldReg);
-            }
-          }
-
-          EntryBlk.splice(InsertionPt, &MBB, MI);
-          Changed = true;
-        }
-        break; 
-      
-      case RISCV::BMOVS_I:
-        // Do dependecne analysis and hoist it as far as we can.
-
-        ++Cbmovs;
-      
-        break;
-
-      case RISCV::BMOVT_J:
-        // You can hoist this to the beginning if the function. 
-        EntryBlk.splice(InsertionPt, &MBB, MI);
-        
-        // EntryBlk.splice(InsertionPt, &MBB, MI);
-        ++Cbmovt;
-        break;
-
-      case RISCV::BMOVT_I:
-        // Do dependecne analysis and hoist it as far as we can.
-        // Pass MI, MBB to a function that returns 'where', which can be 
-        // inserted below. 
-
-        ++Cbmovt;
-        break;
-
-      case RISCV::BMOVC_BNE: {
-        const int cnt = MI.getNumOperands();
-        const MachineOperand &MO = MI.getOperand(0);
-        // if (MO.isReg())
-        //   dbgs() << "BMOVC Operand 0 is a register: " << printReg(MO.getReg(), TRI) << "\n";
-
-        // dbgs() << "BMOVC Operand Count: " << cnt << "\n";
-        ++Cbmovc;
-        break;
-      }
-      case RISCV::BMOVC_BEQ:
-        ++Cbmovc;
-        break;
-
-      case RISCV::BMOVC_BGE:
-        ++Cbmovc;
-        break;
-
-      case RISCV::BMOVC_BGEU:
-        ++Cbmovc;
-        break;
-
-      case RISCV::BMOVC_BLT:
-        ++Cbmovc;
-        break;
-
-      case RISCV::BMOVC_BLTU:
-        ++Cbmovc;
-        break;
-
-      case RISCV::BMOVC_BITS:
-        ++Cbmovc;
-        break;
-
-      case RISCV::BMOVC_LOOP:
-        ++Cbmovc;
-        break;
-
-      case RISCV::PseudoPBC:
-        ++Cpb;
-        break;
-
-      case RISCV::PseudoPBU:
-        ++Cpb;
-        break;
-      }
-    }
-
-    // // Ignore these for now, Just wanted to see  how things works ;>
-    // LLVM_DEBUG(dbgs() << "BMOVS Count: " << Cbmovs << "\n");
-    // LLVM_DEBUG(dbgs() << "BMOVT Count: " << Cbmovt << "\n");
-    // LLVM_DEBUG(dbgs() << "BMOVC Count: " << Cbmovc << "\n");
-    // LLVM_DEBUG(dbgs() << "PB Count: " << Cpb << "\n");
-
-    return Changed;
-  }
+  
 };
 } // end anonymous namespace
 
